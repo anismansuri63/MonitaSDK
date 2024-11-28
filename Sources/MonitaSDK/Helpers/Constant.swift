9d4ccc5b96841b2e2eeaf638274e45e6c54efade
anismansuri63 <anismansuri63@gmail.com>
2024-11-28 18:12:25 +0530

    new code pushed

---
+++ Sources/MonitaSDK/Helpers/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  Monita
+//
+//  Created by Anis Mansuri on 11/09/24.
+//
+
+import Foundation
+struct Constant {
+    ////static let bundle = "com.AppGlobaliOS.sdk"
+    static let bundle = "com.authentication.app"
+}
---
+++ Sources/MonitaSDK/Helpers/URLRequestFactory.swift
@@ -0,0 +1,61 @@
+//
+//  URLRequestFactory.swift
+//  NetworkInterceptor
+//
+// MIT License
+// 
+// Copyright (c) 2018 Kenneth Poon
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+
+class URLRequestFactory {
+    public func createURLRequest(originalUrlRequest: URLRequest, url: URL) -> URLRequest {
+        var urlString = "\(url.absoluteString)\(originalUrlRequest.url!.path)"
+        if let query = originalUrlRequest.url?.query {
+            urlString = "\(urlString)?\(query)"
+        }
+        var redirectedRequest = URLRequest(url: URL(string: urlString)!)
+        if let _ = originalUrlRequest.httpBodyStream,
+           let httpBodyStreamData = originalUrlRequest.getHttpBodyStreamData() {
+            redirectedRequest.httpBody = httpBodyStreamData
+        } else {
+            redirectedRequest.httpBody = originalUrlRequest.httpBody
+        }
+        redirectedRequest.httpMethod = originalUrlRequest.httpMethod!
+        redirectedRequest.allHTTPHeaderFields = originalUrlRequest.allHTTPHeaderFields
+        redirectedRequest.cachePolicy = originalUrlRequest.cachePolicy
+        return redirectedRequest
+    }
+    
+    public func createURLRequest(originalUrlRequest: URLRequest) -> URLRequest {
+        var redirectedRequest = URLRequest(url: originalUrlRequest.url!)
+        if let _ = originalUrlRequest.httpBodyStream,
+           let httpBodyStreamData = originalUrlRequest.getHttpBodyStreamData() {
+            redirectedRequest.httpBody = httpBodyStreamData
+        } else {
+            redirectedRequest.httpBody = originalUrlRequest.httpBody
+        }
+        redirectedRequest.httpMethod = originalUrlRequest.httpMethod!
+        redirectedRequest.allHTTPHeaderFields = originalUrlRequest.allHTTPHeaderFields
+        redirectedRequest.cachePolicy = originalUrlRequest.cachePolicy
+        return redirectedRequest
+    }
+}
